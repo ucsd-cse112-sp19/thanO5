17e9d07cee9312959be21fd706c4d77ec8f6fdcd
Brian Orensztein <borenszt@ucsd.edu>
2019-04-29 12:56:08 -0700

    Fix style error with space before ()

--- test/sample_test2.js
+++ test/sample_test2.js
@@ -2,7 +2,7 @@ describe('Included matchers:', function() {
   let a = 12;
   let b = a;
 
-  it('The "toBe" matcher compares with ===', function () {
+  it('The "toBe" matcher compares with ===', function() {
     a = 12;
     b = a;
     expect(a).toBe(b);
