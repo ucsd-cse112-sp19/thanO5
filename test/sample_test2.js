715544d90dd0bd73affadc6828dbb28ed9bea133
Brian Orensztein <borenszt@ucsd.edu>
2019-04-29 12:47:03 -0700

    Change test to work with eslint

--- test/sample_test2.js
+++ test/sample_test2.js
@@ -1,9 +1,8 @@
-describe("Included matchers:", function() {
+describe('Included matchers:', function() {
 
-  it("The 'toBe' matcher compares with ===", function() {
-    var a = 12;
-    var b = a;
-
+  it('The "toBe" matcher compares with ===', function() {
+    let a = 12;
+    let b = a;
     expect(a).toBe(b);
     expect(a).not.toBe(null);
   });
