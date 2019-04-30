57482814dd6ec2bd5a8fbfe60c9caf2aad2aee91
John <jol065@ucsd.edu>
2019-04-30 15:17:06 -0700

    help please please

--- karma.conf.js
+++ karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function(config) {
     coverageReporter:{
         'type': 'lcovonly',
         'dir': 'coverage/',
-        'file': 'coverage.txt',
+        'file': 'lcov.info',
     },
 
     // web server port
