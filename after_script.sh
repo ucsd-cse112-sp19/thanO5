780f770cef83f8a9f8d764331399d54f1cfbf66b
Rachel <rbarrow@ucsd.edu>
2019-04-30 17:58:11 -0700

    fixed bash script

--- after_script.sh
+++ after_script.sh
@@ -1,15 +1,8 @@
-if [ $(git log -1 --pretty=%B) != "Automated JSDocs" ] 
+commitmsg=$(git log -1 --pretty=%B)
+
+if [ "$commitmsg" != "Automated JSDocs" ] 
 then
 
-    git branch
-    git remote set-url origin https://DannyFung97:48b5a3343ff75559980d107f1f129ca23b07576f@github.com/ucsd-cse112/thanOS.git
-    git config --global user.email "dannyfung97@gmail.com"
-    git config --global user.name "Danny Fung"
-    git add -A
-    git commit -m "Automated JSDocs"
-    git pull
-    git push origin HEAD:${BRANCH}
-    ./cc-test-reporter format-coverage -t lcov "./coverage/Chrome 62.0.3202 (Linux 0.0.0)/lcov.info"
-    ./cc-test-reporter upload-coverage
+    echo "in if"
 
 fi
