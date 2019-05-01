c70a912f5e3d422c2aa53f442062cdfb5570bf13
Rachel <rbarrow@ucsd.edu>
2019-04-30 22:32:12 -0700

    Change doc generation email (codeclimate fix attempt)

--- after_script.sh
+++ after_script.sh
@@ -2,9 +2,9 @@ if [ "$(git log -1 --pretty=%B)" != "Automated JSDocs" ] && [ "$1" == "false" ]
 then
 
     git branch
-    git remote set-url origin https://DannyFung97:48b5a3343ff75559980d107f1f129ca23b07576f@github.com/ucsd-cse112/thanOS.git
-    git config --global user.email "dannyfung97@gmail.com"
-    git config --global user.name "Danny Fung"
+    git remote set-url origin https://rachelbarrow:d42b6be6025ec634ecbb980b1b9fc7dcee27d947@github.com/ucsd-cse112/thanOS.git
+    git config --global user.email "rbarrow@ucsd.edu"
+    git config --global user.name "Rachel Barrow"
     git add -A
     git commit -m "Automated JSDocs"
     git pull
