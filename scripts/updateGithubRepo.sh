a45f7629151c7b2900f27d84632e1f46a9ddfd24
Brian Orensztein <borenszt@ucsd.edu>
2019-05-08 20:52:48 -0700

    Change to Rachel's info

--- scripts/updateGithubRepo.sh
+++ scripts/updateGithubRepo.sh
@@ -1,7 +1,7 @@
 git branch
-git remote set-url origin https://DannyFung97:48b5a3343ff75559980d107f1f129ca23b07576f@github.com/ucsd-cse112/thanOS.git
-git config --global user.email "dannyfung97@gmail.com"
-git config --global user.name "Danny Fung"
+git remote set-url origin https://rachelbarrow:d42b6be6025ec634ecbb980b1b9fc7dcee27d947@github.com/ucsd-cse112/thanOS.git
+git config --global user.email "rbarrow@ucsd.edu"
+git config --global user.name "Rachel Barrow"
 git add -A
 git commit -m "Automated JSDocs with [skip ci]"
 git pull
