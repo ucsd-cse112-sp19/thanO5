a311b135ead0b99c0ce4d1cc9bb78ec7f5c6c320
Brian Orensztein <borenszt@ucsd.edu>
2019-05-10 18:57:10 -0700

    Remove unused config for now

--- karma.conf.js
+++ karma.conf.js
@@ -29,12 +29,6 @@ module.exports = function (config) {
     ],
 
 
-    // list of files / patterns to exclude
-    exclude: [
-      //'components/*.js',
-    ],
-
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
