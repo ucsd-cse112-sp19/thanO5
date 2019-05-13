9eeb1e32c1113966e240a3a0a4c4dc0a7a8ed6ad
k3vn19 <k1barron@ucsd.edu>
2019-05-12 16:34:21 -0800

    Added rounded style to core-button

--- components/core-button/core-button.js
+++ components/core-button/core-button.js
@@ -1,28 +1,32 @@
-/*
-            Notes: Without creating this class the 'my-wc' custom element will just be treated as a
-            container, sort of like a div. 
-        */
-class CoreButton extends HTMLElement { // every HTML element extends HTMLELement 
-    // so my wc must do so as well
-    constructor() {
-        super(); // HTMLElement does class definitions by calling super()
+/**
+ * Core button class.
+ */
+class CoreButton extends HTMLElement {
+  /**
+   * Constructor for CoreButton class.
+   */
+  constructor() {
+    super(); // HTMLElement does class definitions by calling super()
 
-        // create a shadowdom to encapsulate data inside of the custom elements
-        // private document. This data will not be accessed by the rest of the 
-        // html DOM.
-        let shadowdom = this.attachShadow({ mode: 'open' });
-        //let template = document.getElementById('my-wc');
 
-        const template = document.createElement('template');
-        const templateString = '<button id="my-bt" type="button"><slot></slot></button>';
-        template.innerHTML = templateString;
+    const shadowdom = this.attachShadow({ mode: 'open' });
 
-        let templateHTML = template.content.cloneNode(true);
-        shadowdom.appendChild(templateHTML);
-        
-    }
+    const template = document.createElement('template');
+    const templateString = `
+    <style>
+      .rounded{
+        border-radius:25px;
+        border: 2px solid #73AD21;
+        width: 200px;
+        height: 150px;
+      }
+    </style>
+    <button id="my-bt" type="button"><slot></slot></button>
+    `;
+    template.innerHTML = templateString;
 
+    const templateHTML = template.content.cloneNode(true);
+    shadowdom.appendChild(templateHTML);
+  }
 }
 customElements.define('core-hello', CoreButton);
-
-
