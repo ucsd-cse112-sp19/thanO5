53c8046e4f1eb8184bd4d0816f4b37788f8f4db3
Brian Orensztein <borenszt@ucsd.edu>
2019-05-18 16:47:55 -0700

    Delete Selector

--- browsertests/sample.test.js
+++ browsertests/sample.test.js
@@ -1,9 +1,6 @@
-import {Selector} from 'testcafe';
-
 fixture`Getting Started`
     .page`http://devexpress.github.io/testcafe/example`;
 
 test('My first test', async (t) => {
   // Test code
-  Selector('.core-hello.css');
 });
