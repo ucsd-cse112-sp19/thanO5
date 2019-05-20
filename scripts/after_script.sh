4f871bd1a326a090ce90ee30f4acef35a3b2ec47
Rachel <rbarrow@ucsd.edu>
2019-05-20 00:56:00 -0700

    Replace github token with enviornment variable

--- scripts/after_script.sh
+++ scripts/after_script.sh
@@ -2,7 +2,7 @@ if [ "$(git log -1 --pretty=%B)" != "Automated JSDocs" ] && [ "$1" == "false" ]
 then
 
     git branch
-    git remote set-url origin https://rachelbarrow:d42b6be6025ec634ecbb980b1b9fc7dcee27d947@github.com/ucsd-cse112/thanOS.git
+    git remote set-url origin https://rachelbarrow:$GITHUB_API_KEY@github.com/ucsd-cse112/thanOS.git
     git config --global user.email "rbarrow@ucsd.edu"
     git config --global user.name "Rachel Barrow"
     git add -A
