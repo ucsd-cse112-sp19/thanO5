afc836843ab2a6d5db16453ec524ac4b42b7e2b2
Rachel <rbarrow@ucsd.edu>
2019-05-20 01:15:44 -0700

    removed code climate test reporter from after_script

--- scripts/after_script.sh
+++ scripts/after_script.sh
@@ -1,15 +1,12 @@
 if [ "$(git log -1 --pretty=%B)" != "Automated JSDocs" ] && [ "$1" == "false" ] 
 then
-
     git branch
     git remote set-url origin https://rachelbarrow:$GITHUB_API_KEY@github.com/ucsd-cse112/thanOS.git
     git config --global user.email "rbarrow@ucsd.edu"
     git config --global user.name "Rachel Barrow"
     git add -A
-    git commit -m "Automated JSDocs"
+    git commit -m "Automated JSDoc"
     git pull
     git push origin HEAD:${BRANCH}
-    ./cc-test-reporter format-coverage -t lcov "./coverage/lcov.info"
-    ./cc-test-reporter upload-coverage
 
 fi
