b352fa7ebb9a4c80b5140fa98cb9c224439e9608
Brian Orensztein <borenszt@ucsd.edu>
2019-05-31 14:13:49 -0700

    Reorganize repo and work on tests

--- tests/browser/dont_run_this_yet.js
+++
@@ -1,8 +0,0 @@
-import { Selector } from 'testcafe';
-
-fixture `CoreHello Browser Tests`
-    .page`../../components/core-hello/core-hello.html`;
-
-test('My first test', async (t) => {
-  // Test code
-});
