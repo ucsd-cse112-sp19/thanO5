e50aa2165c0aff9422bb5b52f2ae43ff3f2b2b15
Code-Beast <gyf1209732389@hotmail.com>
2019-06-01 23:37:08 -0700

    Refactored core-badge constructor

--- components/core-badge/core-badge.js
+++ components/core-badge/core-badge.js
@@ -23,91 +23,6 @@ export default class CoreBadge extends HTMLElement {
     this._hidden = this.hasAttribute('hidden') || false;
     this._type = this.getAttribute('type') || undefined;
 
-    Object.defineProperty(this, 'max', {
-      get() {
-        return this._max;
-      },
-      set(val) {
-        if (typeof val === 'number') {
-          this._max = val;
-          this.setAttribute('max', val);
-        } else {
-          this._max = undefined;
-          this.removeAttribute('max');
-        }
-
-        this._updateTemplate();
-      },
-    });
-
-    Object.defineProperty(this, 'isDot', {
-      get() {
-        return this._isDot;
-      },
-      set(val) {
-        if (val === true) {
-          this._isDot = true;
-          this.setAttribute('is-dot', '');
-        } else {
-          this._isDot = false;
-          this.removeAttribute('is-dot');
-        }
-
-        this._updateTemplate();
-      },
-    });
-
-    Object.defineProperty(this, 'hidden', {
-      get() {
-        return this._hidden;
-      },
-      set(val) {
-        if (val === true) {
-          this._hidden = true;
-          this.setAttribute('hidden', '');
-        } else {
-          this._hidden = false;
-          this.removeAttribute('hidden');
-        }
-
-        this._updateTemplate();
-      },
-    });
-
-    Object.defineProperty(this, 'value', {
-      get() {
-        return this._value;
-      },
-      set(val) {
-        if (typeof val === 'string' || typeof val === 'number') {
-          this._value = val;
-          this.setAttribute('value', val);
-        } else {
-          this._value = undefined;
-          this.removeAttribute('value');
-        }
-
-        this._updateTemplate();
-      },
-    });
-
-    Object.defineProperty(this, 'type', {
-      get() {
-        return this._type;
-      },
-      set(val) {
-        if (['primary', 'success', 'warning', 'info', 'danger'].indexOf(val) > -1) {
-          this._type = val;
-          this.setAttribute('type', val);
-        } else {
-          this._type = undefined;
-          this.removeAttribute('type');
-        }
-
-        this._updateTemplate();
-      },
-    });
-
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.shadowRoot.appendChild(style.cloneNode(true));
@@ -116,6 +31,122 @@ export default class CoreBadge extends HTMLElement {
     const observer = new MutationObserver(this._updateTemplate.bind(this));
     observer.observe(this, config);
     this.addEventListener('change', this._updateTemplate.bind(this));
+
+    this._updateTemplate();
+  }
+
+  /**
+   * max getter
+   */
+  get max() {
+    return this._max;
+  }
+
+  /**
+   * max setter
+   * @param {*} val
+   */
+  set max(val) {
+    if (typeof val === 'number') {
+      this._max = val;
+      this.setAttribute('max', val);
+    } else {
+      this._max = undefined;
+      this.removeAttribute('max');
+    }
+
+    this._updateTemplate();
+  }
+
+  /**
+   * isDot getter
+   */
+  get isDot() {
+    return this._isDot;
+  }
+
+  /**
+   * isDot setter
+   * @param {*} val
+   */
+  set isDot(val) {
+    if (val === true) {
+      this._isDot = true;
+      this.setAttribute('is-dot', '');
+    } else {
+      this._isDot = false;
+      this.removeAttribute('is-dot');
+    }
+
+    this._updateTemplate();
+  }
+
+  /**
+   * hidden setter
+   * @param {*} val
+   */
+  get hidden() {
+    return this._hidden;
+  }
+
+  /**
+   * hidden setter
+   * @param {*} val
+   */
+  set hidden(val) {
+    if (val === true) {
+      this._hidden = true;
+      this.setAttribute('hidden', '');
+    } else {
+      this._hidden = false;
+      this.removeAttribute('hidden');
+    }
+
+    this._updateTemplate();
+  }
+
+  /**
+   * value getter
+   */
+  get value() {
+    return this._value;
+  }
+
+  /**
+   * value setter
+   * @param {*} val
+   */
+  set value(val) {
+    if (typeof val === 'string' || typeof val === 'number') {
+      this._value = val;
+      this.setAttribute('value', val);
+    } else {
+      this._value = undefined;
+      this.removeAttribute('value');
+    }
+
+    this._updateTemplate();
+  }
+
+  /**
+   * type getter
+   */
+  get type() {
+    return this._type;
+  }
+
+  /**
+   * type setter
+   * @param {*} val
+   */
+  set type(val) {
+    if (['primary', 'success', 'warning', 'info', 'danger'].indexOf(val) > -1) {
+      this._type = val;
+      this.setAttribute('type', val);
+    } else {
+      this._type = undefined;
+      this.removeAttribute('type');
+    }
 
     this._updateTemplate();
   }
