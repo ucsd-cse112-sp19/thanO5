331e95476945ee676fde6b735afe34633cf397d9
Code-Beast <gyf1209732389@hotmail.com>
2019-06-13 01:14:20 -0700

    Added new branch

--- tests/browser/core-hello.test.js
+++ tests/browser/core-hello.test.js
@@ -3,7 +3,7 @@
 fixture `CoreHello Browser Tests`
     .page`./core-hello.test.html`;
 
-test('My first test', async (t) => {
+test('', async (t) => {
   // Test code
   // const element = Selector('corehello1');
   // const name = Selector('slot[name="name"]');
