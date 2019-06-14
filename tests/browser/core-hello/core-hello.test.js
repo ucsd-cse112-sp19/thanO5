e2a046f6062b0f7de8758e7047012cdd6acba76e
Code-Beast <gyf1209732389@hotmail.com>
2019-06-13 18:05:31 -0700

    Wrote all dummy browser tests

--- tests/browser/core-hello/core-hello.test.js
+++ tests/browser/core-hello/core-hello.test.js
@@ -3,7 +3,7 @@ import {Selector} from 'testcafe';
 fixture `CoreHello Browser Tests`
     .page`./core-hello.test.html`;
 
-test('', async (t) => {
+test('should show mark as the person name', async (t) => {
   const hello = await Selector('#test_hello');
   const name = await hello.find('[slot="name"]').innerText;
   await t.expect(name).eql('Mark');
--- tests/browser/core-modal/core-modal.test.js
+++ tests/browser/core-modal/core-modal.test.js
@@ -11,14 +11,14 @@ const close_button = Selector(() => (
   document.querySelector('#test_modal').shadowRoot.querySelector('.close')
 ));
 
-// test('should hide the modal in the beginning', async(t) => {
-//   await t.expect(modal().getStyleProperty('display')).eql('none');
-// });
-
-test('should show the modal when clicking the button', async(t) => {
-  await t.click(open_button());
-  await t.expect(modal().getStyleProperty('display')).eql('block');
+test('should hide the modal in the beginning', async(t) => {
+  await t.expect(modal().getStyleProperty('display')).eql('none');
 });
+
+// test('should show the modal when clicking the button', async(t) => {
+//   await t.click(open_button());
+//   await t.expect(modal().getStyleProperty('display')).eql('block');
+// });
 
 // test('should hide the modal when click the close sign', async(t) => {
 //   await t.click(close_button());
